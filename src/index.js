d2073bb4a72cb6ddb14607d38c269a38ce72450f
Bahar Shah <bahar@getdigraph.com>
2022-10-10 13:46:47 -0400

    import axios

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-const axios = require('axios').default;
+import axios from 'axios';
 
 const validationURL = "notifications/installation"
 
