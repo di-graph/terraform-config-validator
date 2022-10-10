15b516a2eea4eefc2e3bd7e8ec4f4709ff27689b
Bahar Shah <bahar@getdigraph.com>
2022-10-10 13:26:48 -0400

    small cleanup

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
+  // `tf-plan-json` input defined in action metadata file
   const tfInput = core.getInput('tf-plan-json');
-  console.log(`Hello ${tfInput}!`);
+  console.log(`Input is: ${tfInput}!`);
+
+  // Make API call and set response as output
   core.setOutput("response", "ok");
+
+
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+
 } catch (error) {
   core.setFailed(error.message);
 }
